9c1d67297981fea77fd3bbbba8aed783a06da632
Luigi <l.corsi@hotmail.it>
2019-05-29 16:13:44 +0200

    Prova

---
+++ src/it/uniroma2/pmcsn/project/entity/State.java
@@ -0,0 +1,172 @@
+package it.uniroma2.pmcsn.project.entity;
+
+public class State {
+
+    private int num_task_type2_directed_on_cloud;
+
+    private int num_task_type1_cloud;
+    private int num_task_type2_cloud;
+    private int num_task_type1_cloudlet;
+    private int num_task_type2_cloudlet;
+
+    private int num_task_cloud;
+    private int num_task_directed_on_cloud;
+    private int num_task_cloudlet;
+    private int num_task_arrived;
+    private  int num_task_completed;
+
+    private  int n1;
+    private  int n2;
+    private   int n1_cloud;
+    private  int n2_cloud;
+
+    private   int num_task_type1;
+    private  int num_task_type2;
+
+    private  int num_task_interrupted;
+    int num_job_interr_in_list;
+
+    public int getNum_task_type2_directed_on_cloud() {
+        return num_task_type2_directed_on_cloud;
+    }
+
+    public void setNum_task_type2_directed_on_cloud(int num_task_type2_directed_on_cloud) {
+        this.num_task_type2_directed_on_cloud = num_task_type2_directed_on_cloud;
+    }
+
+    public int getNum_task_type1_cloud() {
+        return num_task_type1_cloud;
+    }
+
+    public void setNum_task_type1_cloud(int num_task_type1_cloud) {
+        this.num_task_type1_cloud = num_task_type1_cloud;
+    }
+
+    public int getNum_task_type2_cloud() {
+        return num_task_type2_cloud;
+    }
+
+    public void setNum_task_type2_cloud(int num_task_type2_cloud) {
+        this.num_task_type2_cloud = num_task_type2_cloud;
+    }
+
+    public int getNum_task_type1_cloudlet() {
+        return num_task_type1_cloudlet;
+    }
+
+    public void setNum_task_type1_cloudlet(int num_task_type1_cloudlet) {
+        this.num_task_type1_cloudlet = num_task_type1_cloudlet;
+    }
+
+    public int getNum_task_type2_cloudlet() {
+        return num_task_type2_cloudlet;
+    }
+
+    public void setNum_task_type2_cloudlet(int num_task_type2_cloudlet) {
+        this.num_task_type2_cloudlet = num_task_type2_cloudlet;
+    }
+
+    public int getNum_task_cloud() {
+        return num_task_cloud;
+    }
+
+    public void setNum_task_cloud(int num_task_cloud) {
+        this.num_task_cloud = num_task_cloud;
+    }
+
+    public int getNum_task_directed_on_cloud() {
+        return num_task_directed_on_cloud;
+    }
+
+    public void setNum_task_directed_on_cloud(int num_task_directed_on_cloud) {
+        this.num_task_directed_on_cloud = num_task_directed_on_cloud;
+    }
+
+    public int getNum_task_cloudlet() {
+        return num_task_cloudlet;
+    }
+
+    public void setNum_task_cloudlet(int num_task_cloudlet) {
+        this.num_task_cloudlet = num_task_cloudlet;
+    }
+
+    public int getNum_task_arrived() {
+        return num_task_arrived;
+    }
+
+    public void setNum_task_arrived(int num_task_arrived) {
+        this.num_task_arrived = num_task_arrived;
+    }
+
+    public int getNum_task_completed() {
+        return num_task_completed;
+    }
+
+    public void setNum_task_completed(int num_task_completed) {
+        this.num_task_completed = num_task_completed;
+    }
+
+    public int getN1() {
+        return n1;
+    }
+
+    public void setN1(int n1) {
+        this.n1 = n1;
+    }
+
+    public int getN2() {
+        return n2;
+    }
+
+    public void setN2(int n2) {
+        this.n2 = n2;
+    }
+
+    public int getN1_cloud() {
+        return n1_cloud;
+    }
+
+    public void setN1_cloud(int n1_cloud) {
+        this.n1_cloud = n1_cloud;
+    }
+
+    public int getN2_cloud() {
+        return n2_cloud;
+    }
+
+    public void setN2_cloud(int n2_cloud) {
+        this.n2_cloud = n2_cloud;
+    }
+
+    public int getNum_task_type1() {
+        return num_task_type1;
+    }
+
+    public void setNum_task_type1(int num_task_type1) {
+        this.num_task_type1 = num_task_type1;
+    }
+
+    public int getNum_task_type2() {
+        return num_task_type2;
+    }
+
+    public void setNum_task_type2(int num_task_type2) {
+        this.num_task_type2 = num_task_type2;
+    }
+
+    public int getNum_task_interrupted() {
+        return num_task_interrupted;
+    }
+
+    public void setNum_task_interrupted(int num_task_interrupted) {
+        this.num_task_interrupted = num_task_interrupted;
+    }
+
+    public int getNum_job_interr_in_list() {
+        return num_job_interr_in_list;
+    }
+
+    public void setNum_job_interr_in_list(int num_job_interr_in_list) {
+        this.num_job_interr_in_list = num_job_interr_in_list;
+    }
+}
