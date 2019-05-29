23ba8fc43fd0e44737bf9bb90eb0d0ee0da5bf6b
davide <salvadore.davide@gmail.com>
2019-05-29 16:39:44 +0200

    CIao

---
+++ src/Constants.java
@@ -0,0 +1,41 @@
+public class Constants {
+
+    static double START = 0.0;
+    static long STOP_SIMULATION = 1000000;
+    static long INF = STOP_SIMULATION*10;
+    static double NO_TIME_LOST = 0.0;
+    static int NUM_BATCH = 20;
+    static double LENGTH_BATCH_TIME = STOP_SIMULATION/NUM_BATCH;
+    static double NUM_MAX_SERVER = 5; //NUM_MAX_SERVER==N,numero massimo di server della cloudlet
+
+     //arrival rate and service rate
+
+    static double ARRIVALONE = 4.0 ;     //lambda1 cloudlet   [task/s]
+    static double ARRIVALTWO =  6.25 ;   //lambda2 cloudlet  [task/s]
+    static double SERVICEONE = 0.45;     //u1 cloudlet       [task/s]
+    static double SERVICETWO = 0.27 ; //u2 cloudlet       [task/s]
+    static double SERVICE_CLOUD_ONE =  0.25  ; //u1 cloud  [task/s]
+    static double SERVICE_CLOUD_TWO = 0.22 ;  //u2 cloud  [task/s]
+    static double SETUP = 0.8; //                         [sec]
+
+    //streams per arrivi e servizi
+    static int STREAM_ARR1 = 0;
+    static int STREAM_ARR2 = 10;
+    static int STREAM_SERV_CLOUDLET1 = 20;
+    static int STREAM_SERV_CLOUDLET2 = 30;
+    static int STREAM_SERV_CLOUD1 = 40;
+    static int STREAM_SERV_CLOUD2 = 50;
+    static int STREAM_SETUP = 60;
+
+    //constants
+    static int TASK_TYPE1 = 1;
+    static int TASK_TYPE2 = 2;
+    static int NOT_INTERRUPTED = 0;
+    static int INTERRUPTED = 1;
+    static int DIRECT_CLOUD = 1;
+    static int NOT_DIRECT_CLOUD = 2;
+    static int DIRECT_CLOUDLET = 3;
+
+    static long SEED_DEFAULT = 991102;
+    static double ALPHA = 0.05;
+}
---
+++ src/it/uniroma2/pmcsn/project/entity/Area.java
@@ -0,0 +1,198 @@
+package it.uniroma2.pmcsn.project.entity;
+
+public class Area {
+
+    public Area() {
+
+    }
+
+    public double getSystem() {
+        return system;
+    }
+
+    public void setSystem(double system) {
+        this.system = system;
+    }
+
+    public double getCloudlet() {
+        return cloudlet;
+    }
+
+    public void setCloudlet(double cloudlet) {
+        this.cloudlet = cloudlet;
+    }
+
+    public double getCloud() {
+        return cloud;
+    }
+
+    public void setCloud(double cloud) {
+        this.cloud = cloud;
+    }
+
+    public double getType1() {
+        return type1;
+    }
+
+    public void setType1(double type1) {
+        this.type1 = type1;
+    }
+
+    public double getType2() {
+        return type2;
+    }
+
+    public void setType2(double type2) {
+        this.type2 = type2;
+    }
+
+    public double getJob_interr() {
+        return job_interr;
+    }
+
+    public void setJob_interr(double job_interr) {
+        this.job_interr = job_interr;
+    }
+
+    public double getCloudlet_type1() {
+        return cloudlet_type1;
+    }
+
+    public void setCloudlet_type1(double cloudlet_type1) {
+        this.cloudlet_type1 = cloudlet_type1;
+    }
+
+    public double getCloudlet_type2() {
+        return cloudlet_type2;
+    }
+
+    public void setCloudlet_type2(double cloudlet_type2) {
+        this.cloudlet_type2 = cloudlet_type2;
+    }
+
+    public double getCloud_type1() {
+        return cloud_type1;
+    }
+
+    public void setCloud_type1(double cloud_type1) {
+        this.cloud_type1 = cloud_type1;
+    }
+
+    public double getCloud_type2() {
+        return cloud_type2;
+    }
+
+    public void setCloud_type2(double cloud_type2) {
+        this.cloud_type2 = cloud_type2;
+    }
+
+    public double getPerm_type2() {
+        return perm_type2;
+    }
+
+    public void setPerm_type2(double perm_type2) {
+        this.perm_type2 = perm_type2;
+    }
+
+    public double getCloudlet_type2_v() {
+        return cloudlet_type2_v;
+    }
+
+    public void setCloudlet_type2_v(double cloudlet_type2_v) {
+        this.cloudlet_type2_v = cloudlet_type2_v;
+    }
+
+    public double getCloudlet_type1_v() {
+        return cloudlet_type1_v;
+    }
+
+    public void setCloudlet_type1_v(double cloudlet_type1_v) {
+        this.cloudlet_type1_v = cloudlet_type1_v;
+    }
+
+    public double getCloud_type1_v() {
+        return cloud_type1_v;
+    }
+
+    public void setCloud_type1_v(double cloud_type1_v) {
+        this.cloud_type1_v = cloud_type1_v;
+    }
+
+    public double getCloud_type2_v() {
+        return cloud_type2_v;
+    }
+
+    public void setCloud_type2_v(double cloud_type2_v) {
+        this.cloud_type2_v = cloud_type2_v;
+    }
+
+    public double getSystem_v() {
+        return system_v;
+    }
+
+    public void setSystem_v(double system_v) {
+        this.system_v = system_v;
+    }
+
+    public double getCloudlet_v() {
+        return cloudlet_v;
+    }
+
+    public void setCloudlet_v(double cloudlet_v) {
+        this.cloudlet_v = cloudlet_v;
+    }
+
+    public double getCloud_v() {
+        return cloud_v;
+    }
+
+    public void setCloud_v(double cloud_v) {
+        this.cloud_v = cloud_v;
+    }
+
+    public double getType1_v() {
+        return type1_v;
+    }
+
+    public void setType1_v(double type1_v) {
+        this.type1_v = type1_v;
+    }
+
+    public double getType2_v() {
+        return type2_v;
+    }
+
+    public void setType2_v(double type2_v) {
+        this.type2_v = type2_v;
+    }
+
+    public double getJob_interr_v() {
+        return job_interr_v;
+    }
+
+    public void setJob_interr_v(double job_interr_v) {
+        this.job_interr_v = job_interr_v;
+    }
+
+    private double system;
+    private double cloudlet;
+    private double cloud;
+    private double type1;
+    private double type2;
+    private double job_interr;
+    private double cloudlet_type1;
+    private double cloudlet_type2;
+    private double cloud_type1;
+    private double cloud_type2;
+    private double perm_type2;
+    private double cloudlet_type2_v;
+    private double cloudlet_type1_v;
+    private double cloud_type1_v;
+    private double cloud_type2_v;
+    private double system_v;
+    private double cloudlet_v;
+    private double cloud_v;
+    private double type1_v;
+    private double type2_v;
+    private double job_interr_v;
+}
