d287b5e7bd534443e2d614c3c2da60132f32929c
davide <salvadore.davide@gmail.com>
2019-05-29 18:33:17 +0200

    CIao

--- src/it/uniroma2/pmcsn/project/entity/utils/NextEvent.java
+++ src/it/uniroma2/pmcsn/project/entity/utils/NextEvent.java
@@ -2,11 +2,19 @@ package it.uniroma2.pmcsn.project.entity.utils;
 
 import it.uniroma2.pmcsn.project.entity.distributions.Distributions;
 
+import java.util.concurrent.CopyOnWriteArrayList;
+
 public class NextEvent {
 
     private int init = 0;
     private double[] mean = new double[]{1 / Constants.ARRIVALONE, 1 / Constants.ARRIVALTWO};
     private double[] arrivals = new double[]{Constants.START, Constants.START};
+    private double service;
+    private double setup;
+    private double serviceCloud;
+
+    private Rngs rngs = new Rngs();
+    private Distributions dist = new Distributions();
 
     public double getArrivalCloudlet(int taskType, int newReplication) {
 
@@ -25,8 +33,7 @@ public class NextEvent {
 
         }
 
-        Rngs rngs = new Rngs();
-        Distributions dist = new Distributions();
+
         double arrival;
         if (init < 2) {
             if (init == 0) {
@@ -55,4 +62,61 @@ public class NextEvent {
         }
         return arrival;
     }
+
+    public double getTimeServiceCloudlet(int taskType){
+
+        if( taskType != Constants.TASK_TYPE1 && taskType != Constants.TASK_TYPE2){
+            //gestisci errore
+        }
+        if(taskType == Constants.TASK_TYPE1){
+            rngs.selectStream(Constants.STREAM_SERV_CLOUDLET1)
+            service = dist.Hyperexponential(1/Constants.SERVICEONE);
+        }else(taskType == Constants.TASK_TYPE2){
+            rngs.selectStream(Constants.STREAM_SERV_CLOUDLET2);
+            service = dist.Hyperexponential(1/Constants.SERVICETWO);
+        }
+
+    }
+
+    public double getTimeServiceCloud(int taskType, int interrupted){
+
+       if(taskType!= Constants.TASK_TYPE1 && taskType != Constants.TASK_TYPE2){
+           // gestiisci l'errore
+       }
+       if( taskType == Constants.TASK_TYPE1) {
+           if (interrupted == Constants.INTERRUPTED) {
+               //gestisci errore
+           }
+           rngs.selectStream(Constants.STREAM_SERV_CLOUD1);
+           serviceCloud = dist.Exponential(1 / Constants.SERVICE_CLOUD_ONE);
+       }else{
+           if(interrupted == Constants.INTERRUPTED){
+
+               setup = getTimeSetup();
+               rngs.selectStream(Constants.STREAM_SERV_CLOUD2);
+               serviceCloud = dist.Exponential(1/Constants.SERVICE_CLOUD_TWO);
+
+           }else{
+               rngs.selectStream(Constants.STREAM_SERV_CLOUD2);
+               serviceCloud = dist.Exponential(1/Constants.SERVICE_CLOUD_TWO);
+           }
+       }
+       return serviceCloud;
+    }
+
+    private double getTimeSetup(){
+
+        double timeSetup;
+        rngs.selectStream(Constants.STREAM_SETUP);
+        timeSetup = dist.Exponential(Constants.SETUP);
+        return timeSetup;
+
+    }
+
+    public void assignTaskToCloudlet( double timeCurrent, int taskType){
+
+        double timeEndService = timeCurrent + getTimeServiceCloudlet(taskType);
+
+    }
+
 }
